16d924c5077c9932ec45f8d4a4e86b9397c9ed00
Salvador Martinez <salvadormartinez022693@gmail.com>
2019-05-01 16:16:03 -0500

    update

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(function (req, res, next) {
   if (!req.session.views) {
     req.session.views = {};
   }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname;
+
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+
+  next();
 });
 
 app.use(passport.initialize());
